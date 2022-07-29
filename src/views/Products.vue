f2033a444d3f99eaf11f184470b44d096dbf2e03
Quindevreede <quindevreede@hotmail.com>
2022-07-29 15:29:21 +0200

    ALmost done with this project working with Sidebar and will make a new branch so app doesnt crash
    icons no longer work, prefer icons in assets, will fix that later

---
+++ src/views/Products.vue
@@ -0,0 +1,15 @@
+<template>
+  <main class="wrapper">
+    <h1>Products</h1>
+  </main>
+
+</template>
+
+<script>
+export default {
+  // eslint-disable-next-line vue/multi-word-component-names
+  name: 'Products',  props: ['inventory', 'addToCart'],
+  components: {
+  }
+}
+</script>
